2e0fffe2595d2f03638e2b3ba10315aa5a64cb0a
Marcelo Jr - MCSJ <111250648+marcelocsjunior@users.noreply.github.com>
2026-06-02 07:04:39 -0300

    Configure learning track module relationship

--- src/MentorLab.Api/Data/MentorLabDbContext.cs
+++ src/MentorLab.Api/Data/MentorLabDbContext.cs
@@ -11,6 +11,10 @@ public class MentorLabDbContext : DbContext
 
     public DbSet<Student> Students => Set<Student>();
 
+    public DbSet<LearningTrack> LearningTracks => Set<LearningTrack>();
+
+    public DbSet<Module> Modules => Set<Module>();
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Student>(entity =>
@@ -35,6 +39,59 @@ public class MentorLabDbContext : DbContext
                 .IsRequired();
 
             entity.HasIndex(student => student.Email);
+        });
+
+        modelBuilder.Entity<LearningTrack>(entity =>
+        {
+            entity.HasKey(track => track.Id);
+
+            entity.Property(track => track.Id)
+                .ValueGeneratedOnAdd();
+
+            entity.Property(track => track.Title)
+                .IsRequired()
+                .HasMaxLength(160);
+
+            entity.Property(track => track.Description)
+                .HasMaxLength(500);
+
+            entity.Property(track => track.IsActive)
+                .IsRequired();
+
+            entity.Property(track => track.CreatedAt)
+                .IsRequired();
+
+            entity.HasMany(track => track.Modules)
+                .WithOne(module => module.LearningTrack)
+                .HasForeignKey(module => module.LearningTrackId)
+                .OnDelete(DeleteBehavior.Restrict);
+        });
+
+        modelBuilder.Entity<Module>(entity =>
+        {
+            entity.HasKey(module => module.Id);
+
+            entity.Property(module => module.Id)
+                .ValueGeneratedOnAdd();
+
+            entity.Property(module => module.Title)
+                .IsRequired()
+                .HasMaxLength(160);
+
+            entity.Property(module => module.Description)
+                .HasMaxLength(500);
+
+            entity.Property(module => module.Order)
+                .IsRequired()
+                .HasColumnName("DisplayOrder");
+
+            entity.Property(module => module.IsActive)
+                .IsRequired();
+
+            entity.Property(module => module.CreatedAt)
+                .IsRequired();
+
+            entity.HasIndex(module => module.LearningTrackId);
         });
     }
 }
